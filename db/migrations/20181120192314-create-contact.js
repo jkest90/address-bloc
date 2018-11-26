1de6d9894c582623ce3c347f5df39258866d9cde
jkest90 <jkest618@gmail.com>
2018-11-26 16:37:05 -0700

    mentor help

---
+++ db/migrations/20181120192314-create-contact.js
@@ -0,0 +1,32 @@
+// File contains JavaScript that is translated into SQL to run database operations related to schema.
+
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Contacts');
+  }
+};
