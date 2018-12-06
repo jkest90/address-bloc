d677fe6dee8ff40768fb46226cd49eaa90047d0c
jkest90 <jkest618@gmail.com>
2018-12-05 17:50:23 -0700

    address-bloc:complete

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -31,6 +31,34 @@ module.exports = class ContactController {
             }
          }
       ];
+      this.searchQuestions = [
+         {
+            type: "input",
+            name: "name",
+            message: "Name of contact to search - ",
+            validate(val) {
+               return val !== "";
+            }
+         }
+      ];
+      this.showContactQuestions = [
+         {
+            type: "list",
+            name: "selected",
+            message: "Please choose from an option below",
+            choices: [
+               "Delete contact",
+               "Main menu"
+            ]
+         }
+      ];
+      this.deleteConfirmQuestions = [
+         {
+            type: "confirm",
+            name: "confirmation",
+            message: "Are you sure you want to delete this contact?"
+         }
+      ];
    }
 
    // POST to the database
@@ -42,6 +70,47 @@ module.exports = class ContactController {
    getContacts() {
       // findAll = Sequelize model method. Query the DB for all contact rows in the contacts table.
       return Contact.findAll();
+   }
+
+   iterativeSearch(contacts, target) {
+      for (let contact of contacts) {
+         if (contact.name.toLowerCase() === target.toLowerCase()) {
+            return contact;
+         }
+      }
+      return null;
+   }
+
+   binarySearch(contacts, target) {
+      let min = 0;
+      let max = contacts.length - 1;
+      let mid;
+
+      while (min <= max) {
+         mid = Math.floor((min + max) / 2);
+         let currentContact = contacts[mid];
+
+         if (currentContact.name > target) {
+            max = mid - 1;
+         } else if (currentContact.name < target) {
+            min = mid - 1;
+         } else {
+            return contacts[mid];
+         }
+      }
+      return null;
+   }
+
+   search(name) {
+      return Contact.findOne({
+         where: {name}
+      });
+   }
+
+   delete(id) {
+      return Contact.destroy({
+         where: {id}
+      })
    }
 
  }
